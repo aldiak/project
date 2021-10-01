04cc71e0448be748f489b1bb7a263a3ec9bfd3c9
aldiak <aldiak95@gmail.com>
2021-10-01 13:51:08 +0800

    First LR experiment

---
+++ studentpredictor/train_model.py
@@ -0,0 +1,15 @@
+import pickle
+
+import pandas as pd
+from config import Config
+from sklearn.linear_model import LinearRegression
+
+Config.MODELS_PATH.mkdir(parents=True, exist_ok=True)
+
+X_train = pd.read_csv(str(Config.FEATURES_PATH / "train_features.csv"))
+y_train = pd.read_csv(str(Config.FEATURES_PATH / "train_labels.csv"))
+
+
+model = LinearRegression()
+model = model.fit(X_train, y_train.to_numpy().ravel())
+pickle.dump(model, open(str(Config.MODELS_PATH / "model.pickle"), "wb"))
